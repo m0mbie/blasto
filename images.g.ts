a5e0708c9ffaf97fb1a98a490d66f8b63298bc20
Sgstratax <76879928+Sgstratax@users.noreply.github.com>
2023-08-04 08:46:42 -0600

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,101 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . 1 . . . . . . . . 1 . . . 
+. . 7 1 1 7 7 7 7 7 7 1 1 7 . . 
+. . 7 1 1 1 7 7 7 7 1 1 1 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 f f f f 7 7 f f f f 7 . . 
+. . 7 7 1 2 f 7 7 f 2 1 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 f 7 7 f 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 1 f 1 f f 1 f 1 7 7 . . 
+. . 7 f 1 f f f f f f 1 f 7 . . 
+. . 7 f f f f f f f f f f 7 . . 
+. . 7 f f f f f f f f f f 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+`;
+            case "image2":
+            case "myImage0":return img`
+. . . 1 . . . . . . . . 1 . . . 
+. . 7 1 1 7 7 7 7 7 7 1 1 7 . . 
+. . 7 1 1 1 7 7 7 7 1 1 1 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 f f f f 7 7 f f f f 7 . . 
+. . 7 7 1 2 f 7 7 f 2 1 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 f 7 7 f 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 f 1 f f f f f f 1 f 7 . . 
+. . 7 f f f f f f f f f f 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+`;
+            case "image3":
+            case "myImage1":return img`
+. . . 1 . . . . . . . . 1 . . . 
+. . 7 1 1 7 7 7 7 7 7 1 1 7 . . 
+. . 7 1 1 1 7 7 7 7 1 1 1 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 f f f f 7 7 f f f f 7 . . 
+. . 7 7 1 2 f 7 7 f 2 1 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 f 7 7 f 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 f f 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+`;
+            case "image4":
+            case "myImage2":return img`
+. . . 1 . . . . . . . . 1 . . . 
+. . 7 1 1 7 7 7 7 7 7 1 1 7 . . 
+. . 7 1 1 1 7 7 7 7 1 1 1 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 f f f 7 7 f f f 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 f 7 7 f 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 f f 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+. . 7 7 7 7 7 7 7 7 7 7 7 7 . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
